b55ae8f168eefb1268ae7d1b6fda54748f96eb23
THONWELLING <thonwellingdani13@gmail.com>
2022-09-27 17:51:09 -0300

    utilizando o for

---
+++ src/controle/For.java
@@ -0,0 +1,9 @@
+package controle;
+
+public class For {
+  public static void main(String[] args) {
+    for (int i = 0; i <=10; i++) {
+      System.out.printf("i = %d\n", i);
+    }
+  }
+}
---
+++ src/controle/While.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class While {
+  public static void main(String[] args) {
+     int count = 1;
+
+     while (count <=10) {
+       System.out.println("Bom dia!!!");
+       count++;
+     }
+  }
+}
