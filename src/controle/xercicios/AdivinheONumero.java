584b76ed5254e1a7f2752b1e754374eae8934010
THONWELLING <thonwellingdani13@gmail.com>
2022-10-14 17:56:43 -0300

    adicionado Jogo da advinhação

---
+++ src/controle/xercicios/AdivinheONumero.java
@@ -0,0 +1,48 @@
+package controle.xercicios;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 6. Jogo da adivinhação: Tentar adivinhar um número entre 0 - 100. Armazene
+ * um número aleatório em uma variável. O Jogador tem 10 tentativas para
+ * adivinhar o número gerado. Ao final de cada tentativa, imprima a quantidade
+ * de tentativas restantes, e imprima se o número inserido é maior ou menor do
+ * que o número armazenado.
+ */
+
+public class AdivinheONumero {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int continuar;
+    int numeroSorteado;
+    int tentativas;
+    int numeroDigitado;
+
+    do {
+      System.out.println("Sorteando o número... ");
+      Random numAleatorio = new Random(); // gerando um número aleatório
+      numeroSorteado = numAleatorio.nextInt(101); // aqui é o limite de números como começa do 0 preciso por 101.
+
+      System.out.println("Começou !!! Tente Adivinhar o Número Sorteado...");
+      tentativas = 0;
+
+      do {
+        tentativas++;
+        System.out.printf("Tentativa %d: ", tentativas);
+        numeroDigitado = entrada.nextInt();
+
+        if (numeroDigitado < numeroSorteado) {
+          System.out.printf("\nO número sorteado é maior que %d\n\n", numeroDigitado);
+        } else if (numeroDigitado > numeroSorteado) {
+          System.out.printf("\nO número sorteado é menor que %d\n\n", numeroDigitado);
+        } else {
+          System.out.printf("\nParabéns você acertou o número sorteado em %d tentativas!!\n\n", tentativas);
+
+        }
+      } while (numeroDigitado != numeroSorteado);
+      System.out.println("Digite 0 para sair, ou qualquer outro número para continura: ");
+      continuar = entrada.nextInt();
+    } while (continuar != 0);
+  }
+}
