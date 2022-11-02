054e4a04968f0247194eaf4b9a21d19612593d50
THONWELLING <thonwellingdani13@gmail.com>
2022-11-02 00:39:49 -0300

    adicionando exemplo de relação um pra um uni-direcional e bi-direcional

---
+++ src/oo/composicao/CarroTeste.java
@@ -0,0 +1,40 @@
+package oo.composicao;
+
+public class CarroTeste {
+  public static void main(String[] args) {
+
+    /*
+    * Bidirecional
+    * Motor motor;
+    *
+    * Carro() {
+    *   this.motor = new Motor(this);
+    * }
+    * */
+
+    Carro carro1 = new Carro();
+    System.out.println(carro1.estaLigado());
+
+    carro1.ligar();
+    System.out.println(carro1.estaLigado());
+
+    System.out.println(carro1.motor.giros());
+
+    carro1.acelerar();
+    carro1.acelerar();
+    carro1.acelerar();
+    carro1.acelerar();
+    System.out.println(carro1.motor.giros());
+
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    carro1.frear();
+    System.out.println(carro1.motor.giros());
+  }
+}
---
+++ src/oo/composicao/Motor.java
@@ -0,0 +1,28 @@
+package oo.composicao;
+
+public class Motor {
+
+  /*
+  * Relacionamento Bidirecinal
+  *
+  * Carro carro;
+  * */
+  boolean ligado = false;
+  double fatorInjecao = 1;
+
+  /*
+      Bidirecional
+    * Motor(Carro carro) {
+    * this.carro = carro;}
+  * */
+
+  int giros() {
+    if(!ligado){
+      return 0;
+    } else {
+      return (int) Math.round(fatorInjecao * 3000);
+    }
+  }
+}
+
+
