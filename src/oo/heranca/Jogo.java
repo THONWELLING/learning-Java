e90570023412a60218882e6a3100d37ff98722df
THONWELLING <thonwellingdani13@gmail.com>
2022-11-10 00:52:28 -0300

    adicionando estudos sobre Enum

---
+++ src/oo/heranca/Jogo.java
@@ -0,0 +1,19 @@
+package oo.heranca;
+
+public class Jogo {
+  public static void main(String[] args) {
+
+    Jogador jogador1 = new Jogador();
+
+    jogador1.x = 10;
+    jogador1.y = 20;
+
+    jogador1.andar(Direcao.NORTE);
+    jogador1.andar(Direcao.LESTE);
+    jogador1.andar(Direcao.NORTE);
+    jogador1.andar(Direcao.LESTE);
+
+    System.out.println(jogador1.x);
+    System.out.println(jogador1.y);
+  }
+}
